d0531b1ac737affd788e8d7d2448f759561f0fe4
TaydenLouw <tayden@bbd.co.za>
2019-08-15 08:56:11 +0200

    Module 5

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -12,6 +16,47 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+
+            var orbitingObject = _context.CelestialObjects.FirstOrDefault(x => x.OrbitedObjectId == celestialObject.Id);
+            if (orbitingObject != null)
+            {
+                celestialObject.Satellites = new List<CelestialObject> {orbitingObject};
+            }
+
+            return Ok(celestialObject);
+        }
+        
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObject = _context.CelestialObjects.Where(x => x.Name == name).ToList();
+            if (celestialObject.Count == 0)
+            {
+                return NotFound();
+            }
+
+            celestialObject.ForEach(x => x.Satellites = new List<CelestialObject> {_context.CelestialObjects.FirstOrDefault(k => k.OrbitedObjectId == x.Id)});
+
+
+            return Ok(celestialObject);
+        }
+        
+        [HttpGet( Name = "GetAll")]
+        public IActionResult GetAll()
+        {
+            var celestialObject = _context.CelestialObjects.ToList();
+            celestialObject.ForEach(x => x.Satellites = new List<CelestialObject> {_context.CelestialObjects.FirstOrDefault(k => k.OrbitedObjectId == x.Id)});
+            return Ok(celestialObject);
         }
     }
 }
